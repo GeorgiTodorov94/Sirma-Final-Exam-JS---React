bdd337495c4136c88f07f13f9d313dcce28534e5
Your Name <you@example.com>
2024-09-11 20:31:32 +0300

    Add Match Card Field

--- src/Components/fields/Field.jsx
+++ src/Components/fields/Field.jsx
@@ -6,6 +6,7 @@ import '../../styling/field.css';
 import { useData } from '../../utilities/dataContext';
 import FieldA from './FieldA';
 import FieldB from './FieldB';
+import MatchFieldCard from '../match-card/MatchFieldCard';
 
 export default function Field({
     match,
@@ -25,9 +26,21 @@ export default function Field({
             teams: teams.data,
             currentMatch: match,
             clubPlayers: groupedPlayersByTeamID
+            // Need to implement better logic in getting clubPlayers.
         }));
     }, [match, teams.data, matches.data, players.data, matchID]);
     console.log(currentData.clubPlayers)
+    // I need to clean most of the components if i have time 
+    // I will start from here. This Logic here is taking all the info 
+    // dynamically about the current match and converts it into one object with
+    // key value pairs = {
+    // match: {match}
+    // players: {players}
+    // team: {team}
+    //    }
+    // I can create a custom hook and implement it everywhere
+    // where any match information is needed for manipulation
+    // clean code and much cleaner components
 
 
     if (!currentData.teams?.length || !currentData.currentMatch || !currentData.clubPlayers) {
@@ -68,6 +81,7 @@ export default function Field({
         <div className='fields-container'>
             <div className="fields">
                 <FieldA key={matchDetails.teamA.ID} team={matchDetails.teamA} players={matchDetails.teamAPlayers} match={match} />
+                <MatchFieldCard key={matchDetails.teamA.ID} team={matchDetails} aTeamPlayers={matchDetails.teamAPlayers} bTeamPlayers={matchDetails.teamBPlayers} match={match} />
                 <FieldB key={matchDetails.teamB.ID} team={matchDetails.teamB} players={matchDetails.teamBPlayers} match={match} />
             </div>
 
---
+++ src/Components/match-card/MatchFieldCard.jsx
@@ -0,0 +1,52 @@
+import { useNavigate, useParams } from "react-router"
+
+import "../../styling/matchFieldCard.css"
+
+export default function MatchFieldCard({
+    team,
+    ...match
+}) {
+    const navigate = useNavigate();
+    console.log(team)
+    const [ATeamScore, BTeamScore] = match.match.Score.split('-');
+
+    return (
+        <div className="field-match-card">
+            <div className="fieldTeam team1">
+                <img src="" />
+                <div className="field-team-info">
+                    <h3>{team.teamA.Name}</h3>
+                    <h3 style={{ fontSize: '10px' }}>
+                        Club Manager  -
+                    </h3>
+                    {team.teamA.ManagerFullName}
+                </div>
+            </div>
+
+            <div className="match-info">
+                <div style={{ padding: '10px' }} className={`field-match-result ${match.match.Score === 'WIN' ? 'win' : ''}`}>
+                    <h3>Played On</h3>
+                    {`${match.match.Date}`}
+                    {/* Win */}
+                </div>
+                <div className="field-score">
+                    <span>{ATeamScore}</span> : <span>{BTeamScore}</span>
+                </div>
+
+
+                {/* <button className="field-home-label" onClick={() => navigate(`/match-details/${match.match.ID}`)}>Match Details</button> */}
+            </div>
+
+            <div className="fieldTeam team2">
+                <img src="" />
+                <div className="field-team-info">
+                    <h3>{team.teamB.Name}</h3>
+                    <h3 style={{ fontSize: '10px' }}>
+                        Club Manager  -
+                    </h3>
+                    {team.teamB.ManagerFullName}
+                </div>
+            </div>
+        </div>
+    );
+};
